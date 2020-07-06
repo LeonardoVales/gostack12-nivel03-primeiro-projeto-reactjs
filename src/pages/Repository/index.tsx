3dde5973ec08df116d7eb324516e3fb9158d6369
Leonardo Vales <leohenrique.vales@gmail.com>
2020-07-06 08:28:13 -0300

    Detalhes do repositório

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,17 +1,101 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom'; // fornece os parâmetros da rota
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom'; // fornece os parâmetros da rota
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import LogoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
+
+  const [repository, setRepository] = useState(null);
+  const [issues, setIssues] = useState([]);
+
+  // useEffect causa um "efeito"
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      console.log(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      console.log(response.data);
+    });
+
+    // async function loadData(): Promise<void> {
+    //   const [repository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`),
+    //   ]);
+
+    //   console.log(repository);
+    //   console.log(issues);
+    // }
+
+    // loadData();
+  }, [params.repository]);
+
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={LogoImg} alt="Gihub Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>45</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
